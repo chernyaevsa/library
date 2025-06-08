2e7c2be3ee8bd02348f6a5b0b921ff01eec7c5bb
Черняев Сергей <csa@czsa.ru>
2025-06-08 19:32:52 +0500

    Добавлена модель Book и базовые роуты и исправлена ошибка циклического import

--- server.py
+++ server.py
@@ -2,18 +2,38 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
+from extensions import db
+import models
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app, )
+db.init_app(app)
 migrate = Migrate(app, db)
 
 @app.route("/")
 def index():
     return render_template("index.html", current_page="main")
 
+@app.route('/books')
+def books():
+    books = models.Book.query.all()
+    return render_template('books.html', books=books)
+
+@app.route('/books/add', methods=['POST'])
+def add_book():
+    if request.method == 'POST':
+        book = Book(
+            title=request.form['title'],
+            author=request.form['author'],
+            year=request.form['year']
+        )
+        db.session.add(book)
+        db.session.commit()
+        return redirect(url_for('books'))
+
 def main():
     app.run("0.0.0.0", port=5000, debug=True)
+
 
 if __name__ == "__main__":
     main()
