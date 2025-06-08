12f3844d78a502fabf34c4cab54000cdb30a2fcf
Черняев Сергей <csa@czsa.ru>
2025-06-08 17:49:36 +0500

    Инициализация проекта

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app, )
+migrate = Migrate(app, db)
+
+@app.route("/")
+def index():
+    return render_template("index.html", current_page="main")
+
+def main():
+    app.run("0.0.0.0", port=5000, debug=True)
+
+if __name__ == "__main__":
+    main()
